67b0f852d9be99c78fea32bf098961204666fa04
Steve VandenBrink <steve@vandenbrinksoftware.com>
2024-09-10 23:29:42 -0400

    Created new basket project using vertical slice, CQRS pattern from building blocks, adding feature for shipping cart (create, update, get, delete).

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+
+    internal class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            // TODO: Get basket from the database
+            // var basket = await _repository.GetBasket(query.UserName);
+
+            return new GetBasketResult(new ShoppingCart("swn"));
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasktHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace Basket.API.Basket.StoreBasket
+{
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+
+    public class StoreBasketCommandVaidator : AbstractValidator<StoreBasketCommand>
+    {
+        public StoreBasketCommandVaidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart cannot be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+
+    internal class StoreBasktHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = command.Cart;
+
+            // TODO: store basket in database
+            // TODO: update cache
+
+            return new StoreBasketResult("swn");
+        }
+    }
+}
